ed669a99d20730e0eaf5df7f9338f92b76b05b74
Brandon Madison <brandmadis@gmail.com>
2023-11-14 19:39:37 -0800

    Init firebase

--- src/pages/teamRegistration.js
+++ src/pages/teamRegistration.js
@@ -1,7 +1,20 @@
+import { db } from '../firebaseConfig.js';
+import { doc, setDoc } from "firebase/firestore";
+
 function TeamRegistration() {
+  // Add a new document in collection "cities"
+  const button = async () => {
+    await setDoc(doc(db, "cities", "LA"), {
+      name: "Los Angeles",
+      state: "CA",
+      country: "USA"
+    });
+  }
+
   return (
     <>
-        <h1>Team Registration</h1>
+      <h1>Team Registration</h1>
+      <button onClick={button}>Click me</button>
     </>
   );
 }
