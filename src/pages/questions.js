b1187364647fb374d2acffe35589886799815cae
JINDOUJIANG <jindoujiang@outlook.com>
2023-12-21 20:12:56 -0800

    click on question toggle display or hide for questions page

--- src/pages/questions.js
+++ src/pages/questions.js
@@ -1,6 +1,19 @@
 import NavComponent from "./navComponent";
+import React, { useState } from "react";
 
 function Questions() {
+    const [questionVisibility, setQuestionVisibility] = useState({
+        question1: true,
+        question2: true,
+        question3: true,
+    });
+
+    const toggleQuestionVisibility = (question) => {
+        setQuestionVisibility((prevVisibility) => ({
+            ...prevVisibility, [question]: !prevVisibility[question],
+        }));
+    };
+
     return (
         <>
             <NavComponent />
@@ -16,8 +29,12 @@ function Questions() {
                     </thead>
                     <tbody>
                         <tr>
-                            <td>Question1</td>
-                            <td><button>OFF/ON</button></td>
+                            <td className="has-text-success">
+                                <a onClick={() => toggleQuestionVisibility('question1')}>Question1</a>
+                            </td>
+                            <td>
+                                <button>OFF/ON</button>
+                            </td>
                             <td>
                                 <div className="select is-link">
                                     <select>
@@ -27,8 +44,70 @@ function Questions() {
                                 </div>
                             </td>
                         </tr>
+                        {questionVisibility.question1 && (
+                            <>
+                                <p>Was Satan from Hell?</p>
+                                <p><b>Correct Answer(s): No</b></p>
+                                <table className="table is-bordered">
+                                    <thead>
+                                        <tr>
+                                            <td style={{ paddingRight: '40px' }}>Team</td>
+                                            <td style={{ paddingRight: '40px' }}>Answer</td>
+                                            <td style={{ paddingRight: '40px' }}>Correct?</td>
+                                            <td style={{ paddingRight: '40px' }}>Points</td>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Team1</td>
+                                            <td>no</td>
+                                            <td>
+                                                <label className="checkbox">
+                                                    <input type='checkbox' />
+                                                </label>
+                                            </td>
+                                            <td>
+                                                <label className="points">
+                                                    <input type='number' style={{ width: '40px' }} />
+                                                </label>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Team2</td>
+                                            <td>maybe</td>
+                                            <td>
+                                                <label className="checkbox">
+                                                    <input type='checkbox' />
+                                                </label>
+                                            </td>
+                                            <td>
+                                                <label className="points">
+                                                    <input type='number' style={{ width: '40px' }} />
+                                                </label>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Team3</td>
+                                            <td>yes</td>
+                                            <td>
+                                                <label className="checkbox">
+                                                    <input type='checkbox' />
+                                                </label>
+                                            </td>
+                                            <td>
+                                                <label className="points">
+                                                    <input type='number' style={{ width: '40px' }} />
+                                                </label>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </>
+                        )}
                         <tr>
-                            <td>Question2</td>
+                            <td className="has-text-primary">
+                                <a onClick={() => toggleQuestionVisibility('question2')}>Question2</a>
+                            </td>
                             <td><button>OFF/ON</button></td>
                             <td>
                                 <div className="select is-link">
@@ -39,64 +118,70 @@ function Questions() {
                                 </div>
                             </td>
                         </tr>
-                        <p>Did Adam have a belly button?</p>
-                        <p><b>Correct Answer(s): Maybe</b></p>
-                        <table className="table is-bordered">
-                            <thead>
-                                <tr>
-                                    <td style={{ paddingRight: '40px' }}>Team</td>
-                                    <td style={{ paddingRight: '40px' }}>Answer</td>
-                                    <td style={{ paddingRight: '40px' }}>Correct?</td>
-                                    <td style={{ paddingRight: '40px' }}>Points</td>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Team1</td>
-                                    <td>maybe</td>
-                                    <td>
-                                        <label className="checkbox">
-                                            <input type='checkbox' />
-                                        </label>
-                                    </td>
-                                    <td>
-                                        <label className="points">
-                                            <input type='number' style={{ width: '40px' }} />
-                                        </label>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Team2</td>
-                                    <td>yes</td>
-                                    <td>
-                                        <label className="checkbox">
-                                            <input type='checkbox' />
-                                        </label>
-                                    </td>
-                                    <td>
-                                        <label className="points">
-                                            <input type='number' style={{ width: '40px' }} />
-                                        </label>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Team3</td>
-                                    <td>no</td>
-                                    <td>
-                                        <label className="checkbox">
-                                            <input type='checkbox' />
-                                        </label>
-                                    </td>
-                                    <td>
-                                        <label className="points">
-                                            <input type='number' style={{ width: '40px' }} />
-                                        </label>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        {questionVisibility.question2 && (
+                            <>
+                                <p>Did Adam have a belly button?</p>
+                                <p><b>Correct Answer(s): Maybe</b></p>
+                                <table className="table is-bordered">
+                                    <thead>
+                                        <tr>
+                                            <td style={{ paddingRight: '40px' }}>Team</td>
+                                            <td style={{ paddingRight: '40px' }}>Answer</td>
+                                            <td style={{ paddingRight: '40px' }}>Correct?</td>
+                                            <td style={{ paddingRight: '40px' }}>Points</td>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Team1</td>
+                                            <td>maybe</td>
+                                            <td>
+                                                <label className="checkbox">
+                                                    <input type='checkbox' />
+                                                </label>
+                                            </td>
+                                            <td>
+                                                <label className="points">
+                                                    <input type='number' style={{ width: '40px' }} />
+                                                </label>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Team2</td>
+                                            <td>yes</td>
+                                            <td>
+                                                <label className="checkbox">
+                                                    <input type='checkbox' />
+                                                </label>
+                                            </td>
+                                            <td>
+                                                <label className="points">
+                                                    <input type='number' style={{ width: '40px' }} />
+                                                </label>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Team3</td>
+                                            <td>no</td>
+                                            <td>
+                                                <label className="checkbox">
+                                                    <input type='checkbox' />
+                                                </label>
+                                            </td>
+                                            <td>
+                                                <label className="points">
+                                                    <input type='number' style={{ width: '40px' }} />
+                                                </label>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </>
+                        )}
                         <tr>
-                            <td>Question3</td>
+                            <td className="has-text-danger">
+                                <a onClick={() => toggleQuestionVisibility('question3')}>Question3</a>
+                            </td>
                             <td><button>OFF/ON</button></td>
                             <td>
                                 <div className="select is-link">
@@ -107,6 +192,66 @@ function Questions() {
                                 </div>
                             </td>
                         </tr>
+                        {questionVisibility.question3 && (
+                            <>
+                                <p>Panda from China?</p>
+                                <p><b>Correct Answer(s): Yes</b></p>
+                                <table className="table is-bordered">
+                                    <thead>
+                                        <tr>
+                                            <td style={{ paddingRight: '40px' }}>Team</td>
+                                            <td style={{ paddingRight: '40px' }}>Answer</td>
+                                            <td style={{ paddingRight: '40px' }}>Correct?</td>
+                                            <td style={{ paddingRight: '40px' }}>Points</td>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Team1</td>
+                                            <td>yes</td>
+                                            <td>
+                                                <label className="checkbox">
+                                                    <input type='checkbox' />
+                                                </label>
+                                            </td>
+                                            <td>
+                                                <label className="points">
+                                                    <input type='number' style={{ width: '40px' }} />
+                                                </label>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Team2</td>
+                                            <td>no</td>
+                                            <td>
+                                                <label className="checkbox">
+                                                    <input type='checkbox' />
+                                                </label>
+                                            </td>
+                                            <td>
+                                                <label className="points">
+                                                    <input type='number' style={{ width: '40px' }} />
+                                                </label>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Team3</td>
+                                            <td>maybe</td>
+                                            <td>
+                                                <label className="checkbox">
+                                                    <input type='checkbox' />
+                                                </label>
+                                            </td>
+                                            <td>
+                                                <label className="points">
+                                                    <input type='number' style={{ width: '40px' }} />
+                                                </label>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </>
+                        )}
                     </tbody>
                 </table>
             </div>
